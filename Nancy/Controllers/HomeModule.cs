2d1ae6c8ff12fe9050f0deeec6fe96eadf02c55f
Jorge Fernandez <info@erpbox.net>
2018-12-10 06:26:12 -0800

    first commit

---
+++ Nancy/Controllers/HomeModule.cs
@@ -0,0 +1,34 @@
+﻿using Nancy;
+using NancyMS.Model;
+using Nancy.ModelBinding;
+using Nancy.Validation;
+using System.Threading.Tasks;
+
+namespace NancyMS.Controllers
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello World, it's Nancy on .NET Core"); 
+
+            Get("/test/{name}", args => new Person() { Name = args.name });
+
+            Post("/person", async (args,_) =>
+            {
+                Person model = this.Bind();
+                var result = this.Validate(model);
+
+                await Task.Delay(8000);
+                if (!result.IsValid)
+                {
+                    return Response.AsJson(result, HttpStatusCode.UnprocessableEntity);
+                    //return 422;
+                }
+
+                return 200;
+            });
+        }
+
+    }
+}
---
+++ Nancy/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+
+namespace NancyMS.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().Length(3,10);
+            this.RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            this.RuleFor(x => x.Age).GreaterThanOrEqualTo(18);
+        }
+    }
+}
